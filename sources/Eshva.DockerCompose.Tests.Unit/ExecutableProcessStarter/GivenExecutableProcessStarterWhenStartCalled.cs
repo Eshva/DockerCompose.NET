c139f64c8dface9e8066064ffd8fa94c6f54255a
Mike Eshva <reg@eshva.ru>
2019-12-20 01:31:00 +0300

    dcn #001 fix start process tests
    These tests for some reason halt on AppVeyor.

--- sources/Eshva.DockerCompose.Tests.Unit/ExecutableProcessStarter/GivenExecutableProcessStarterWhenStartCalled.cs
+++ sources/Eshva.DockerCompose.Tests.Unit/ExecutableProcessStarter/GivenExecutableProcessStarterWhenStartCalled.cs
@@ -24,7 +24,7 @@ namespace Eshva.DockerCompose.Tests.Unit.ExecutableProcessStarter
         [Fact]
         public void ShouldBreakExecutionAfterSpecifiedAmountOfTime()
         {
-            var starter = new Infrastructure.ExecutableProcessStarter("docker-compose");
+            var starter = new Infrastructure.ExecutableProcessStarter("arp");
             Func<Task> execute = async () => await starter.Start(string.Empty, TimeSpan.FromMilliseconds(1));
             execute.Should().ThrowExactly<TimeoutException>();
         }
@@ -32,16 +32,16 @@ namespace Eshva.DockerCompose.Tests.Unit.ExecutableProcessStarter
         [Fact]
         public async Task ShouldCaptureProcessStandardOutput()
         {
-            var starter = new Infrastructure.ExecutableProcessStarter("docker");
-            await starter.Start("--version", TimeSpan.FromDays(1));
+            var starter = new Infrastructure.ExecutableProcessStarter("arp");
+            await starter.Start("-a", TimeSpan.FromDays(1));
             starter.StandardOutput.ReadToEnd().Length.Should().BeGreaterThan(0);
         }
 
         [Fact]
         public async Task ShouldCaptureProcessStandardError()
         {
-            var starter = new Infrastructure.ExecutableProcessStarter("docker");
-            await starter.Start(string.Empty, TimeSpan.FromDays(1));
+            var starter = new Infrastructure.ExecutableProcessStarter("arp");
+            await starter.Start("-s sdsfsddsf adsdsfsdf", TimeSpan.FromDays(1));
             starter.StandardError.ReadToEnd().Length.Should().BeGreaterThan(0);
         }
     }
